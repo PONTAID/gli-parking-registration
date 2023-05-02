e69fb6f400f010d6bbab93bd26f91af492db28a1
Klaudius Carol I B <91418707+carol2540@users.noreply.github.com>
2023-05-02 14:56:33 +0700

    File build

--- src/global_variable/global_variable.js
+++
@@ -1,9 +0,0 @@
-// const api_url = "http://34.87.97.244:5001/wo/";
-const api_url = "https://development.gli.id:5002/api/parking/";
-// const api_url = "http://127.0.0.1:5000/parking/";
-// const api_url_gocorp = "http://127.0.0.1:5000/gocorp/";
-const api_url_gocorp = "https://development.gli.id:5002/api/gocorp/";
-
-
-export { api_url };
-export { api_url_gocorp };
